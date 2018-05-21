4d90becc933effe495447623a4af97af97a67080
errorrmaxim <dmaks@debian.synapse.com>
2018-05-22 02:17:22 +0300

    [dev-manager] Add sending messages by mosqitto

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "connectionmanager.h"
+
+const std::string kHost = "localhost";
+const int kPort = 1883;
+
 
 int main() {
     std::cout << "Hello initial commit!\n";
+    ConnectionManager a("127.0.0.1", 1883);
+    a.send_message("esp1", "ON");
 	return 0;
 }
