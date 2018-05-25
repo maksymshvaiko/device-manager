8c0827b024e7a65ce800659119f8feec929eedbd
errorrmaxim <errorrmaxim@gmail.com>
2018-05-26 00:27:28 +0300

    [dev-manager] Delete copy and move semantic
    - remove copy and move semantic for class 'ConnectionManager'

--- include/connectionmanager.h
+++ include/connectionmanager.h
@@ -7,7 +7,15 @@ const int QOS1 = 1;
 
 class ConnectionManager : public mosqpp::mosquittopp {
 public:
-    ConnectionManager(const std::string &host, int port);
+    ConnectionManager(const ConnectionManager &) = delete;
+    ConnectionManager &operator =(const ConnectionManager &) = delete;
+    ConnectionManager(ConnectionManager &&) = delete;
+    ConnectionManager &operator =(ConnectionManager &&) = delete;
+
+    static ConnectionManager &instance(const std::string &host, int port) {
+        static ConnectionManager inst(host, port);
+        return inst;
+    }
     ~ConnectionManager();
 
     bool send_message(const std::string &topic, const std::string &message);
@@ -16,6 +24,7 @@ public:
 private:
     bool connected_{};
 
+    ConnectionManager(const std::string &host, int port);
     void async_connect(const char *host, int port, int keepalive = 60);
     void on_connect(int rc);
     void on_publish(int mid);
